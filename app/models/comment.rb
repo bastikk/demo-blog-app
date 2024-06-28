92db7b132bb2489face4380877c226237de32e7e
Ivan Bastylo <Ivan_Bastylo@epam.com>
2024-06-28 14:11:34 +0300

    Added comments import script

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,8 @@
 class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user
-  
+
+  PROHIBITED_KEYWORDS = ["rerum", "mollitia", "exercitationem", "eum", "omnis", "et", "corporis", "provident", "consequuntur", "qui", "repudiandae", "dolorem", "optio", "recusandae", "assumenda", "pariatur", "nostrum", "unde", "ut", "quia"]
   
   
   validate :content_does_not_contain_prohibited_keywords
@@ -10,8 +11,9 @@ class Comment < ApplicationRecord
   private
 
   def content_does_not_contain_prohibited_keywords
-    prohibited_keywords = 20.times.map { Faker::Lorem.unique.word }
-    prohibited_keywords.each do |keyword|
+    # prohibited_keywords = 20.times.map { Faker::Lorem.unique.word }
+
+    PROHIBITED_KEYWORDS.each do |keyword|
       if content.downcase.include?(keyword)
         errors.add(:content, "contains prohibited keyword: #{keyword}")
         break
---
+++ import_comments_from_csv.rb
@@ -0,0 +1,40 @@
+require 'csv'
+require_relative './config/environment'
+
+def import(csv_file_path)
+  CSV.foreach(csv_file_path, headers: true) do |row|
+    post = Post.find_by(slug: row['Post Slug'])
+    user = User.find_or_create_by(name: row['User Name'], email: row['User Email'])
+
+    if post
+      comment = Comment.new(
+        content: row['Content'],
+        post: post,
+        user: user
+      )
+
+      unless comment.save
+        # puts "Failed to import comment for post: #{post.title}. Errors: #{comment.errors.full_messages.join(', ')}"
+        # Decided to write everything in log file as it is to much errors
+        log_error("Failed to import comment for post: #{post.title}. Errors: #{comment.errors.full_messages.join(', ')}")
+      end
+    else
+      log_error("Post with slug '#{row['Post Slug']}' not found.")
+    end
+  end
+end
+
+
+def log_error(message)
+  File.open('import_comments_log.txt', 'a') do |file|
+    file.puts(message)
+  end
+end
+
+csv_file_path = 'comments.csv'.freeze
+import(csv_file_path)
+
+# Read the comments.csv file.
+# For each row in the CSV, find the corresponding Post by its slug.
+# Create a new Comment associated with the found Post
+# Handle and report any validation errors
