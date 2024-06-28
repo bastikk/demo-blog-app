d3a6adb09e344a61d0caff1a3b5e8652219ddb44
Ivan Bastylo <Ivan_Bastylo@epam.com>
2024-06-28 13:38:14 +0300

    Fixed issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  # Your existing code...
+
+  def current_user
+    # Mocking current_user for demonstration. Replace with your actual authentication method.
+    User.first
+  end
 end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,8 +15,7 @@ class CommentsController < ApplicationController
   private
 
   def set_post
-    binding.pry
-    @post = Post.find(params[:post_id])
+    @post = Post.find_by!(slug: params[:post_slug])
   end
 
   def comment_params
